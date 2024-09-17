d85db9cadb57ca7c54afe3757c52a8ae9b7251b6
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-17 04:34:56 +0300

    Added QuestionController class

---
+++ src/main/java/com/willis/question_service/controller/QuestionController.java
@@ -0,0 +1,37 @@
+package com.willis.question_service.controller;
+
+import com.willis.Quiz.App.Demo.model.Question;
+import com.willis.Quiz.App.Demo.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+    @GetMapping("/allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category){
+        return questionService.getQuestionByCategory(category);
+    }
+    @PostMapping("/add")
+    public ResponseEntity<String> addQuestions(@RequestBody Question question){
+        return questionService.addQuestions(question);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteQuestions(@PathVariable Integer id){
+        return questionService.deleteQuestions(id);
+    }
+    @PutMapping("/update")
+    public ResponseEntity<String> updateQuestions(@RequestBody Question question){
+        return questionService.updateQuestions(question);
+    }
+}
