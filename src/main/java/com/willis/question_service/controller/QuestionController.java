232a34bebdcfa751ff3461789141982a0c0c051f
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-17 04:46:45 +0300

    Controller:Rectified imports

--- src/main/java/com/willis/question_service/controller/QuestionController.java
+++ src/main/java/com/willis/question_service/controller/QuestionController.java
@@ -1,7 +1,8 @@
 package com.willis.question_service.controller;
 
-import com.willis.Quiz.App.Demo.model.Question;
-import com.willis.Quiz.App.Demo.service.QuestionService;
+
+import com.willis.question_service.model.Question;
+import com.willis.question_service.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
