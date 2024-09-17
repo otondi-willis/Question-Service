9262a3499358f5a2c38176810227e9ea3d2fdb72
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-17 04:36:00 +0300

    DAO:Added QuestionDao class

---
+++ src/main/java/com/willis/question_service/dao/QuestionDao.java
@@ -0,0 +1,14 @@
+package com.willis.question_service.dao;
+
+import com.willis.Quiz.App.Demo.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface QuestionDao extends JpaRepository<Question,Integer> {
+    List<Question> findByCategory(String category);
+
+    @Query(value="SELECT * FROM question q Where q.category=:category ORDER BY RANDOM()", nativeQuery = true)
+    List<Question> findRandomQuestionsByCategory(String category);
+}
