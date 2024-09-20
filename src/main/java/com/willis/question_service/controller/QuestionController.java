dc4c00a8fea8cc668267b28afe5f7b901622e500
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-20 04:51:16 +0300

    method to get environment an instance is running on

--- src/main/java/com/willis/question_service/controller/QuestionController.java
+++ src/main/java/com/willis/question_service/controller/QuestionController.java
@@ -5,6 +5,8 @@ import com.willis.question_service.model.Question;
 import com.willis.question_service.model.QuestionWrapper;
 import com.willis.question_service.model.Response;
 import com.willis.question_service.service.QuestionService;
+//import org.hibernate.cfg.Environment;
+import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +14,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/question")
+@RequestMapping("question")
 public class QuestionController {
 
     @Autowired
     QuestionService questionService;
+
+    @Autowired
+    Environment environment;
+
+
     @GetMapping("/allQuestions")
     public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
@@ -47,6 +54,7 @@ public class QuestionController {
     //getQuestions (questionId)
     @PostMapping("getQuestions")
     public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds){
+        System.out.println(environment.getProperty("local.server.port"));
         return questionService.getQuestionsFromId(questionIds);
     }
     //getScore
